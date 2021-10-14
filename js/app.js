83ef2490889526379a276929366bcbdad442afe8
tamer_maarouf <tamermaarouf@gmail.com>
2021-10-14 22:32:43 +0200

    create function to Highlighting links

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@ function initAll(){                                                             
     const sections = document.querySelectorAll('section');                                              // Get all section in document 
     const navmenu = document.getElementById('navbar__list');                                            // Get ul By ID and store in var
     const fragment = document.createDocumentFragment();                                                 // create documnet fragment
-    createNavBar(sections, navmenu, fragment);                                                          // call createNavBar() Function
+    createNavBar(sections, navmenu, fragment); 
 
     document.addEventListener('scroll', () => {                                                         // Add Scroll Event Listener to to the document
       // This line call checkIsVisible() function to check if section in the view
-      checkIsVisible(sections);                             
-    })
+      checkIsVisible(sections);      
+    })                                                        // call createNavBar() Function
 
   }else{
     //Add Code Here
@@ -27,9 +27,8 @@ function createNavBar(allSection, navBAr, frag){
   const sections = allSection;
   const navBarList = navBAr;
   const docFragment = frag;
-  
+
   sections.forEach(section => {
-  
     const navList = document.createElement('li');               // create li list 
     const navListSub = document.createElement('a');             // create link
     const navListSubText = document.createTextNode(section.dataset.nav); // Add data-* Attr from 
@@ -37,17 +36,16 @@ function createNavBar(allSection, navBAr, frag){
     navListSub.setAttribute('data-link',section.id);
   
     navListSub.classList.add('menu__link');
-  
     navListSub.appendChild(navListSubText);
     navList.appendChild(navListSub);
     docFragment.appendChild(navList);
   
     navListSub.addEventListener('click', () => {
     section.scrollIntoView({'behavior':'smooth'});
-    })
+  })
   
   });
-  navBarList.appendChild(docFragment);
+  navBarList.appendChild(docFragment);  
 }
 
 /*
@@ -56,15 +54,32 @@ This Function Check is section visible in view
 */ 
 const checkIsVisible = (elements) => {
 
-for(let element of elements){  
-  let rect = element.getBoundingClientRect();
-  if(rect.y >= 0 && (rect.y <= 200 && rect.y < window.innerHeight)){ 
-    element.classList.add('your-active-class');                                 //Add Class to section 
-  }else{
-    element.classList.remove('your-active-class');                              //Remove Class to section
+  for(let element of elements){ 
+    let rect = element.getBoundingClientRect();
+    if(rect.y >= 0 && (rect.y <= 120 && rect.y < window.innerHeight)){ 
+      element.classList.add('your-active-class');                                 //Add Class to section
+      activeLinks(element); 
+    }else{
+      element.classList.remove('your-active-class');                              //Remove Class to section
+    }
+  
+  } // End For Loop 
+  
+  } // End Function 
+
+const activeLinks = (e) => {
+
+  const activeLink = document.querySelectorAll('[data-link]');
+  const element = e;
+  for(let i = 0; i < activeLink.length; i++){
+    
+    if(activeLink[i].textContent === element.dataset.nav){
+      activeLink[i].classList.add('menu-link-active');
+      console.log(activeLink[i].textContent);
+    }else{
+      activeLink[i].classList.remove('menu-link-active');
+    }
   }
 
-} // End For Loop 
-
-} // End Function 
-
+}
+  
